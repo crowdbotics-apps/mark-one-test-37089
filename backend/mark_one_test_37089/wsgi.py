9b6924d6c61586416778bafd529963d87be5f040
Crowdbotics <team@crowdbotics.com>
2022-09-19 12:19:16 +0000

    Update models

--- backend/mark_one_test_37089/wsgi.py
+++ backend/mark_one_test_37089/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mark_one_test_37089.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mark_one_test_37089.settings")
 
 application = get_wsgi_application()
