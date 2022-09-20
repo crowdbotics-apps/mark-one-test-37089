baaed1bafa3ac7cef5c077dd735b08d31095c3ec
Crowdbotics <team@crowdbotics.com>
2022-09-20 20:16:16 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,20 @@
+from django.conf import settings
 from django.db import models
+
+
+class UserProfile(models.Model):
+    "Generated Model"
+    telephone = models.BigIntegerField()
+    occupation = models.CharField(
+        max_length=256,
+    )
+    profile_picture = models.CharField(
+        max_length=256,
+    )
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="userprofile_user",
+    )
